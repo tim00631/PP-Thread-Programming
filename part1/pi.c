b4987d854cdec5c2049d64a6cb5b7b4d66fd0410
“yuhao” <tim00631@gmail.com>
2020-11-05 03:21:48 +0800

    avx512 target flag

--- part1/pi.c
+++ part1/pi.c
@@ -68,11 +68,12 @@ void* child_thread(void* args) {
     {
         const __m256d unit = _mm256_set_pd(1, 1, 1, 1);
         // generate 32 random bytes
+
         __m256d random_x = _mm256_cvtepi64_pd(avx_xorshift128plus(&key));
         __m256d random_y = _mm256_cvtepi64_pd(avx_xorshift128plus(&key));
         __m256d max = _mm256_set_pd(LONG_MAX,LONG_MAX,LONG_MAX,LONG_MAX);
         __m256d x = _mm256_div_pd(random_x, max);
-        __m256d y = _mm256_div_pd(random_x, max);
+        __m256d y = _mm256_div_pd(random_y, max);
         __m256d square_x = _mm256_mul_pd(x, x);
         __m256d square_y = _mm256_mul_pd(y, y);
         __m256d square_sum = _mm256_add_pd(square_x, square_y);
